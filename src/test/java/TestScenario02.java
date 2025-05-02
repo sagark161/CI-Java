8ed058e45ce6dedeb1f31f85e1eaa4c02ae35bb6
sagark161 <sagar.k161@gmail.com>
2025-05-02 23:42:37 +0530

    Test - a tests updated

--- src/test/java/TestScenario02.java
+++ src/test/java/TestScenario02.java
@@ -5,7 +5,7 @@ public class TestScenario02 {
 
     @Test
     void test03() {
-        Assert.assertEquals(1 + 1, 2, "Test03 to pass");
+        Assert.assertEquals(1 + 1, 2, "Test03 to PASS");
     }
 
     @Test
